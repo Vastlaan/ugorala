80dfeff48ac42354fdd7ca5276d5479f431246b1
vastlaan <vastlaan@mail.com>
2021-06-02 15:55:47 +0200

    Created folder footer[test]

---
+++ client/components/footer/index.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export default function FooterComponent() {
+    return (
+        <Footer>
+            
+        </Footer>
+    )
+}
+
+const Footer = styled.footer`
+    height: 50rem;
+    background-color: ${p=>p.theme.grey4};
+`
